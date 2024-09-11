ac15e871a72da4b54fc8456f30be4f83474a2a6e
Isaac Thoman <isaacthoman@outlook.com>
2024-09-11 14:05:06 -0400

    threejs works

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-// Serve static files from the 'public' directory
 app.use(express.static(join(__dirname, 'public')));
 
-// This route is optional if you have index.html in public directory
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'public', 'index.html'));
-});
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected 🐙');
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected 🐙');
     });
 });
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000 🐙');
 });
-
