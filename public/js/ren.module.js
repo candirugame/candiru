08de13dfdf5917fe6245a8ceefad77fc3355625d
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-09-14 23:25:39 -0400

    fixed vertical resolution, fog

--- public/js/ren.module.js
+++ public/js/ren.module.js
@@ -1,4 +1,5 @@
 import * as THREE from './three.module.js';
+import * as NETWORKING  from './networking.module.js';
 
 const scene = new THREE.Scene();
 
@@ -7,8 +8,9 @@ const clock = new THREE.Clock();
 const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.01, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
+renderer.domElement.style.imageRendering = 'pixelated';
+onWindowResize();
 
 window.addEventListener('resize', onWindowResize, false);
 
@@ -18,12 +20,16 @@ export function doFrame(localPlayer){
     renderer.render( scene, camera );
 
     camera.position.copy(localPlayer.position)
+
+    scene.fog = new THREE.FogExp2('#111111',0.1)
+
 }
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(200/window.innerHeight);
 }
 
 export function getScene() {
