8cb2737ad76c5ec0e28a3bd406b9dd72598257eb
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-09-15 18:08:33 -0400

    start of remote player rendering

--- src/js/ren.module.js
+++ src/js/ren.module.js
@@ -3,7 +3,6 @@ import * as NETWORKING  from './networking.module.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import {DRACOLoader} from "three/addons/loaders/DRACOLoader.js";
 
-
 const scene = new THREE.Scene();
 
 const clock = new THREE.Clock();
@@ -20,6 +19,8 @@ const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/draco/');
 loader.setDRACOLoader( dracoLoader );
 
+let possumGLTFScene = undefined;
+
 
 // Load a glTF resource
 loader.load(
@@ -27,27 +28,18 @@ loader.load(
     'models/simplified_possum.glb',
     // called when the resource is loaded
     function ( gltf ) {
-        scene.add( gltf.scene );
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Group
-        gltf.scenes; // Array<THREE.Group>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
+        possumGLTFScene = gltf.scene;
+        console.log( possumGLTFScene );
         },
     function ( xhr ) {},
-    function ( error ) {console.log( 'An error happened' );}
+    function ( error ) {console.log( 'possum loading error' );}
 );
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
+scene.fog = new THREE.FogExp2('#111111',0.1)
 
-
-
-
-
-onWindowResize();
-
-window.addEventListener('resize', onWindowResize, false);
+let playersToRender = [];
 
 export function doFrame(localPlayer){
     let deltaTime = clock.getDelta();
@@ -56,10 +48,75 @@ export function doFrame(localPlayer){
 
     camera.position.copy(localPlayer.position)
 
-    scene.fog = new THREE.FogExp2('#111111',0.1)
+    updateRemotePlayers();
 
 }
 
+
+function updateRemotePlayers() {
+    if (possumGLTFScene === undefined) return;
+
+    console.log(playersToRender);
+
+    let remotePlayerData = NETWORKING.getRemotePlayerData();
+
+    // Update existing players and add new players
+    for (let i = 0; i < remotePlayerData.length; i++) {
+        let playerFound = false;
+
+        for (let j = 0; j < playersToRender.length; j++) {
+            if (remotePlayerData[i]['id'] === playersToRender[j]['id']) {
+                // Update player position
+                playersToRender[j]['object'].position.set(
+                    remotePlayerData[i]['position'].x,
+                    remotePlayerData[i]['position'].y,
+                    remotePlayerData[i]['position'].z
+                );
+                playerFound = true;
+                break;
+            }
+        }
+
+        if (!playerFound) {
+            // Add new player
+            let newPlayer = {
+                id: remotePlayerData[i]['id'],
+                object: possumGLTFScene.clone() // Clone the GLTF scene for the new player
+            };
+            newPlayer.object.position.set(
+                remotePlayerData[i]['position'].x,
+                remotePlayerData[i]['position'].y,
+                remotePlayerData[i]['position'].z
+            );
+            playersToRender.push(newPlayer);
+            scene.add(newPlayer.object);
+        }
+    }
+
+    // Remove players that are no longer in remotePlayerData
+    for (let i = playersToRender.length - 1; i >= 0; i--) {
+        let playerFound = false;
+
+        for (let j = 0; j < remotePlayerData.length; j++) {
+            if (remotePlayerData[j]['id'] === playersToRender[i]['id']) {
+                playerFound = true;
+                break;
+            }
+        }
+
+        if (!playerFound) {
+            scene.remove(playersToRender[i]['object']);
+            playersToRender.splice(i, 1);
+        }
+    }
+}
+
+
+
+
+
+onWindowResize();
+window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
