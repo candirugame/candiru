c1c57a6870264a51afa4c09bcd4bd605d0fbabee
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-09-21 16:01:19 -0400

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "node_modules/", // Ignore node_modules directory
+      "dist/",        // Ignore dist directory
+      "*.min.js",     // Ignore all minified JavaScript files
+      "public/",
+    ],
+  },
+];
