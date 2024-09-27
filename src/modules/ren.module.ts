3fac7760c33d2c995e2600a7f115846a3264fcd6
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-09-26 22:01:00 -0400

    move chat to own scene for render ordering

--- src/modules/ren.module.ts
+++ src/modules/ren.module.ts
@@ -8,7 +8,6 @@ import * as CHAT from './canvasOverlay.module.ts';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.01, 1000);
 const renderer = new THREE.WebGLRenderer();
-
 document.body.appendChild(renderer.domElement);
 renderer.domElement.style.imageRendering = 'pixelated';
 renderer.setAnimationLoop(null);
@@ -19,16 +18,14 @@ dracoLoader.setDecoderPath('/draco/');
 loader.setDRACOLoader(dracoLoader);
 
 let possumGLTFScene = undefined;
-
 loader.load(
     'models/simplified_possum.glb',
     function (gltf) {
         possumGLTFScene = gltf.scene;
     },
-    function () {},
+    function () { },
     function () { console.log('possum loading error'); }
 );
-
 
 let playersToRender = [];
 
@@ -45,7 +42,7 @@ const cube = new THREE.Mesh(geometry, material);
 cube.position.set(1, 1, 1); // Ensure the cube is at the origin
 // heldItemScene.add(cube);
 
-export function getHeldItemScene(){
+export function getHeldItemScene() {
     return heldItemScene;
 }
 
@@ -53,37 +50,32 @@ export function getHeldItemScene(){
 cube.renderOrder = 999;
 cube.material.depthTest = false;
 
-
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 const ambientLight2 = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 heldItemScene.add(ambientLight2);
-
-heldItemScene.fog = new THREE.FogExp2('#111111',0.1);
+heldItemScene.fog = new THREE.FogExp2('#111111', 0.1);
 scene.fog = new THREE.FogExp2('#111111', 0.1);
-
-
 
 export function doFrame(localPlayer) {
     // Render the main scene
     renderer.render(scene, camera);
-
     renderer.autoClear = false;
 
     // Ensure chat elements are always on top
-    const chatScene = CHAT.getScene();
+    const chatScene = CHAT.getChatScene();
+    const chatCamera = CHAT.getChatCamera();
+
     chatScene.traverse((obj) => {
         if (obj.isMesh) {
-            obj.renderOrder = 998;  // Ensure it's rendered just before the held item
+            obj.renderOrder = 998; // Ensure it's rendered just before the held item
             obj.material.depthTest = false;
         }
     });
 
-    renderer.render(chatScene, camera);
 
-    // Render the held item scene
     renderer.render(heldItemScene, heldItemCamera);
-
+    renderer.render(chatScene, chatCamera);
     renderer.autoClear = true;
 
     camera.position.copy(localPlayer.position);
@@ -136,15 +128,18 @@ function updatePlayerPosition(playerObject, remotePlayerData) {
         remotePlayerData.position.y,
         remotePlayerData.position.z
     );
+
     playerObject.quaternion.set(
         remotePlayerData.quaternion[0],
         remotePlayerData.quaternion[1],
         remotePlayerData.quaternion[2],
         remotePlayerData.quaternion[3]
     );
+
     const velocity = Math.sqrt(Math.pow(remotePlayerData.velocity.x, 2) + Math.pow(remotePlayerData.velocity.y, 2) + Math.pow(remotePlayerData.velocity.z, 2));
     if (velocity > 0)
         playerObject.position.add(new THREE.Vector3(0, 0.2 * (0.5 + Math.sin(Date.now() / 1000 * 20)), 0));
+
     // Apply additional rotation
     const rotationQuaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
     playerObject.quaternion.multiply(rotationQuaternion);
@@ -180,9 +175,12 @@ function onWindowResize() {
 
     // Update held item camera aspect ratio
     heldItemCamera.aspect = window.innerWidth / window.innerHeight;
-    console.log(heldItemCamera.fov);
-
     heldItemCamera.updateProjectionMatrix();
+
+    // Update chat camera aspect ratio
+    const chatCamera = CHAT.getChatCamera();
+    chatCamera.aspect = window.innerWidth / window.innerHeight;
+    chatCamera.updateProjectionMatrix();
 }
 
 export function getScene() {
