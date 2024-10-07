3220ffb0658eaed4774e3f7c1b308c86baae84a1
AustinJMann <77241134+AustinJMann@users.noreply.github.com>
2024-10-07 00:28:34 -0400

    feat: sphere based collisions!

--- src/CollisionManager.ts
+++ src/CollisionManager.ts
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 import { Renderer } from './Renderer';
 import { Player } from './Player';
-import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree } from 'three-mesh-bvh';
+import {acceleratedRaycast, computeBoundsTree, disposeBoundsTree, StaticGeometryGenerator, MeshBVH } from 'three-mesh-bvh';
+import {Group, Vector3} from "three";
 
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
 THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
@@ -9,12 +10,16 @@ THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 
 export class CollisionManager {
     private clock: THREE.Clock;
+    private colliderSphere: THREE.Sphere;
+    private deltaVec: THREE.Vector3;
     private raycaster: THREE.Raycaster;
     private scene: THREE.Scene;
     private gravity: number;
     private physicsTickRate: number;
     private physicsTickTimer: number;
-    public static mapLoaded: boolean = false;
+    public mapLoaded: boolean = false;
+    private staticGenerator: StaticGeometryGenerator;
+    private colliderGeom: THREE.BufferGeometry;
 
     constructor(renderer: Renderer, physicsTickRate: number) {
         this.scene = renderer.getScene();
@@ -26,6 +31,8 @@ export class CollisionManager {
             this.physicsTickRate = 1/60;
         }
         this.physicsTickTimer = 0;
+        this.colliderSphere = new THREE.Sphere(new Vector3(), .2);
+        this.deltaVec = new THREE.Vector3();
     }
 
     public init() {
@@ -33,62 +40,69 @@ export class CollisionManager {
     }
 
     public collisionPeriodic(localPlayer: Player) {
-        if(!CollisionManager.mapLoaded) {return;}
+        if(!this.mapLoaded) {return;}
         const deltaTime: number = this.clock.getDelta();
-        // console.log(localPlayer.position);
         this.physics(localPlayer, deltaTime);
     }
 
     private physics(localPlayer: Player, deltaTime: number) {
+
         this.physicsTickTimer += deltaTime;
+
         while(this.physicsTickTimer >= this.physicsTickRate) {
-            let onGround: boolean = false;
 
-            const direction = new THREE.Vector3();
-            localPlayer.velocity.y = 0;
-            direction.copy(localPlayer.velocity);
-            direction.normalize();
+            this.gravity += this.physicsTickRate * - 20;
+            localPlayer.velocity.y += this.gravity;
+            localPlayer.position.add(localPlayer.velocity.clone().multiplyScalar(this.physicsTickRate));
 
-            this.raycaster.set(localPlayer.position, new THREE.Vector3(0, -1, 0));
+            const bvh: MeshBVH = this.colliderGeom.boundsTree;
+            this.colliderSphere.center = localPlayer.position.clone();
 
-            const down_intersects = this.raycaster.intersectObjects(this.scene.children, true);
+            bvh.shapecast( {
 
-            if (down_intersects.length > 0) {
-                for (const intersect of down_intersects) {
-                    if (intersect.distance < 0.201) {
-                        const slopeAngle = Math.acos(intersect.face.normal.dot(new THREE.Vector3(0, 1, 0)));
-                        if (!(slopeAngle < Math.PI / 4 && slopeAngle != 0)) {
-                            localPlayer.position.y = intersect.point.y + 0.2;
-                        }
+                intersectsBounds: box => {
 
-                        onGround = true;
+                    return box.intersectsSphere( this.colliderSphere );
+
+                },
+
+                intersectsTriangle: tri => {
+
+                    // get delta between closest point and center
+                    tri.closestPointToPoint( this.colliderSphere.center, this.deltaVec );
+                    this.deltaVec.sub( this.colliderSphere.center );
+                    const distance = this.deltaVec.length();
+                    if ( distance < this.colliderSphere.radius ) {
+
+                        // move the sphere position to be outside the triangle
+                        const radius = this.colliderSphere.radius;
+                        const depth = distance - radius;
+                        this.deltaVec.multiplyScalar( 1 / distance );
+                        this.colliderSphere.center.addScaledVector( this.deltaVec, depth );
+                        localPlayer.position.addScaledVector( this.deltaVec, depth );
                         this.gravity = 0;
                     }
-                }
-            }
-            this.raycaster.set(localPlayer.position, direction);
-            const intersects = this.raycaster.intersectObjects(this.scene.children, true);
 
-            if (intersects.length > 0) {
-                for (const intersect of intersects) {
-                    if (intersect.distance < 0.2) {
-                        const normal = intersect.face.normal;
-                        const slopeAngle = Math.acos(intersect.face.normal.dot(new THREE.Vector3(0, 1, 0)));
-                        if (!(slopeAngle < Math.PI / 4)) {
-                            normal.y = 0;
-                        }
-                        localPlayer.velocity = localPlayer.velocity.projectOnPlane(normal);
-                    }
-                }
-            }
+                },
 
-            if(!onGround) {
-                this.gravity += -9.8 * deltaTime;
-                localPlayer.position.add(new THREE.Vector3(0, this.gravity, 0).multiplyScalar(this.physicsTickRate));
-            }
+                boundsTraverseOrder: box => {
 
-            localPlayer.position.add(localPlayer.velocity.clone().multiplyScalar(this.physicsTickRate));
+                    return box.distanceToPoint( this.colliderSphere.center ) - this.colliderSphere.radius;
+
+                },
+
+            } );
+
             this.physicsTickTimer -= this.physicsTickRate;
         }
+    }
+
+    public staticGeometry(group: Group) {
+        this.staticGenerator = new StaticGeometryGenerator( group );
+        this.staticGenerator.attributes = [ 'position' ];
+        this.colliderGeom = this.staticGenerator.generate();
+        this.colliderGeom.computeBoundsTree();
+        console.log(this.colliderGeom.boundsTree);
+        this.mapLoaded = true;
     }
 }
