77b157ec81d764d887e604803bdfc5640519fb94
AustinJMann <77241134+AustinJMann@users.noreply.github.com>
2024-10-07 01:48:05 -0400

    feat: physics is now always running at the same speed as your framerate.

--- src/CollisionManager.ts
+++ src/CollisionManager.ts
@@ -15,22 +15,14 @@ export class CollisionManager {
     private raycaster: THREE.Raycaster;
     private scene: THREE.Scene;
     private gravity: number;
-    private physicsTickRate: number;
-    private physicsTickTimer: number;
     public mapLoaded: boolean = false;
     private staticGenerator: StaticGeometryGenerator;
     private colliderGeom: THREE.BufferGeometry;
 
-    constructor(renderer: Renderer, physicsTickRate: number) {
+    constructor(renderer: Renderer) {
         this.scene = renderer.getScene();
         this.clock = new THREE.Clock();
         this.raycaster = new THREE.Raycaster();
-        if (physicsTickRate) {
-            this.physicsTickRate = physicsTickRate;
-        } else {
-            this.physicsTickRate = 1/60;
-        }
-        this.physicsTickTimer = 0;
         this.colliderSphere = new THREE.Sphere(new Vector3(), .2);
         this.deltaVec = new THREE.Vector3();
     }
@@ -46,33 +38,28 @@ export class CollisionManager {
     }
 
     private physics(localPlayer: Player, deltaTime: number) {
+        this.gravity += deltaTime * -20;
+        localPlayer.velocity.y += this.gravity;
+        localPlayer.position.add(localPlayer.velocity.clone().multiplyScalar(deltaTime));
 
-        this.physicsTickTimer += deltaTime;
+        const bvh: MeshBVH = this.colliderGeom.boundsTree;
+        this.colliderSphere.center = localPlayer.position.clone();
 
-        while(this.physicsTickTimer >= this.physicsTickRate) {
+        bvh.shapecast( {
 
-            this.gravity += this.physicsTickRate * - 20;
-            localPlayer.velocity.y += this.gravity;
-            localPlayer.position.add(localPlayer.velocity.clone().multiplyScalar(this.physicsTickRate));
+            intersectsBounds: box => {
 
-            const bvh: MeshBVH = this.colliderGeom.boundsTree;
-            this.colliderSphere.center = localPlayer.position.clone();
+                return box.intersectsSphere( this.colliderSphere );
 
-            bvh.shapecast( {
+            },
 
-                intersectsBounds: box => {
+            intersectsTriangle: tri => {
 
-                    return box.intersectsSphere( this.colliderSphere );
-
-                },
-
-                intersectsTriangle: tri => {
-
-                    // get delta between closest point and center
-                    tri.closestPointToPoint( this.colliderSphere.center, this.deltaVec );
-                    this.deltaVec.sub( this.colliderSphere.center );
-                    const distance = this.deltaVec.length();
-                    if ( distance < this.colliderSphere.radius ) {
+                // get delta between closest point and center
+                tri.closestPointToPoint( this.colliderSphere.center, this.deltaVec );
+                this.deltaVec.sub( this.colliderSphere.center );
+                const distance = this.deltaVec.length();
+                if ( distance < this.colliderSphere.radius ) {
 
                     // move the sphere position to be outside the triangle
                     const radius = this.colliderSphere.radius;
@@ -85,16 +72,14 @@ export class CollisionManager {
                 }
             },
 
-                boundsTraverseOrder: box => {
+            boundsTraverseOrder: box => {
 
-                    return box.distanceToPoint( this.colliderSphere.center ) - this.colliderSphere.radius;
+                return box.distanceToPoint( this.colliderSphere.center ) - this.colliderSphere.radius;
 
-                },
+            },
 
-            } );
+        } );
 
-            this.physicsTickTimer -= this.physicsTickRate;
-        }
     }
 
     public staticGeometry(group: Group) {
