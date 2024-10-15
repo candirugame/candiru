a608e82446ef76c9aa739970d4ca895402cd3ca0
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-10-14 21:34:09 -0400

    start with banana for now

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const playerKickTime = 5; //kick players after 5 seconds of no ping
 const healthRegenRate = 3; //regen 3 health per second
 const healthRegenDelay = 5; //regen after 5 seconds of no damage
 const maxHealth = 100;
-const baseInventory = [];
+const baseInventory = [1];
 
 
 let playerData = [];
