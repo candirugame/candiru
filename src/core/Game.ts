1f01e71722215377bb9120f6fda1753bfbb1b26c
AustinJMann <77241134+AustinJMann@users.noreply.github.com>
2024-10-17 21:20:49 -0400

    changed id to gameIndex

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -20,18 +20,18 @@ export class Game {
     private map: MapLoader;
     private healthIndicator: HealthIndicator;
     private remoteItemRenderer: RemoteItemRenderer;
-    private id: number;
-    private static nextId: number = 0;
+    private gameIndex: number;
+    private static nextGameIndex: number = 0;
 
 
     constructor() {
-        this.id = Game.nextId++;
+        this.gameIndex = Game.nextGameIndex++;
         this.localPlayer = new Player();
         this.chatOverlay = new ChatOverlay(this.localPlayer);
         this.networking = new Networking(this.localPlayer, this.chatOverlay);
         this.renderer = new Renderer(this.networking, this.localPlayer, this.chatOverlay);
         this.chatOverlay.setRenderer(this.renderer);
-        this.inputHandler = new InputHandler(this.renderer, this.localPlayer, this.id);
+        this.inputHandler = new InputHandler(this.renderer, this.localPlayer, this.gameIndex);
         this.collisionManager = new CollisionManager(this.renderer, this.inputHandler);
         this.inventoryManager = new Inventory(this.renderer, this.inputHandler, this.networking, this.localPlayer);
         this.chatOverlay.setNetworking(this.networking);
