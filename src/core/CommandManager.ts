e5856ffbc0f7449d2d68f44210009e162c915c38
AustinJMann <77241134+AustinJMann@users.noreply.github.com>
2024-10-22 21:46:56 -0400

    feat: crosshair customization with /crosshairColor and /crosshairType

--- src/core/CommandManager.ts
+++ src/core/CommandManager.ts
@@ -1,6 +1,7 @@
 import {Player} from "./Player.ts";
 import {ChatOverlay} from "../ui/ChatOverlay.ts";
 import {SettingsManager} from "./SettingsManager.ts";
+import {string} from "joi";
 
 export class CommandManager {
     private localPlayer: Player;
@@ -28,11 +29,37 @@ export class CommandManager {
                 return "Sensitivity is not in the valid range of 0 to 10";
             }
         }));
+
         this.commands.push(new Command('resetSettings', (): string => {
             SettingsManager.reset();
             SettingsManager.write();
             return "Settings have been reverted to their default states";
         }));
+
+        this.commands.push(new Command('crosshairColor', (args: string[]): string => {
+            if (!(args[1] && args[2] && args[3])) {
+                return 'Not an rgb value'
+            }
+
+            for (let i = 1; i < args.length; i++) {
+                if(Number(args[i]) < 0 || Number(args[i]) > 255) {
+                    return args[i] + ' is not in range 0-255';
+                }
+            }
+
+            SettingsManager.settings.crosshairColor = '#' + componentToHex(Number(args[1])) + componentToHex(Number(args[2])) + componentToHex(Number(args[3]));
+            SettingsManager.settings.crosshairColorFlashing = invertRGB(Number(args[1]), Number(args[2]), Number(args[3]));
+            SettingsManager.write();
+            return 'Crosshair color set'
+        }));
+
+        this.commands.push(new Command('crosshairType', (args: string[]): string => {
+            if (args[1] == 'cross') SettingsManager.settings.crosshairType = 0;
+            else if (args[1] == 'dot') SettingsManager.settings.crosshairType = 1;
+            else return 'not a valid type (dot or cross)';
+            SettingsManager.write();
+            return 'Crosshair type set'
+        } ));
     }
 
     public runCmd(cmd: string): boolean {
@@ -71,4 +98,20 @@ class Command {
     public getCmdName() {
         return this.cmdName;
     }
+}
+
+function componentToHex(c: number): string {
+    const hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r: number, g: number, b: number): string {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function invertRGB(r: number, g: number, b: number, a?: number): string {
+    const rgb = [r, g, b, a].map((value, index) => {
+        return index === 3 ? (a !== undefined ? 1 - Number(value) : 1) : 255 - Number(value);
+    });
+    return rgbToHex(rgb[0], rgb[1], rgb[2]);
 }
