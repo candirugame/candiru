aa5bf75cc924c5db734a3db693faa5c45ad16108
AustinJMann <77241134+AustinJMann@users.noreply.github.com>
2024-10-24 14:18:18 -0400

    Can now use css color name to set crosshair color.

--- src/core/CommandManager.ts
+++ src/core/CommandManager.ts
@@ -36,18 +36,27 @@ export class CommandManager {
         }));
 
         this.commands.push(new Command('crosshairColor', (args: string[]): string => {
-            if (!(args[1] && args[2] && args[3])) {
-                return 'Not an rgb value'
-            }
+            if ((args[1] && args[2] && args[3])) {
 
-            for (let i = 1; i < args.length; i++) {
-                if(Number(args[i]) < 0 || Number(args[i]) > 255) {
-                    return args[i] + ' is not in range 0-255';
+
+                for (let i = 1; i < args.length; i++) {
+                    if (Number(args[i]) < 0 || Number(args[i]) > 255) {
+                        return args[i] + ' is not in range 0-255';
+                    }
                 }
-            }
 
-            SettingsManager.settings.crosshairColor = '#' + componentToHex(Number(args[1])) + componentToHex(Number(args[2])) + componentToHex(Number(args[3]));
-            SettingsManager.settings.crosshairColorFlashing = invertRGB(Number(args[1]), Number(args[2]), Number(args[3]));
+                SettingsManager.settings.crosshairColor = '#' + componentToHex(Number(args[1])) + componentToHex(Number(args[2])) + componentToHex(Number(args[3]));
+            } else if (args[1]) {
+                const color = colorNameToHex(args[1]);
+                if(!color) {
+                    return args[1] + ' is not a valid color';
+                }
+
+                SettingsManager.settings.crosshairColor = color;
+            } else {
+                return 'invalid input';
+            }
+            SettingsManager.settings.crosshairColorFlashing = invertColor(SettingsManager.settings.crosshairColor);
             SettingsManager.write();
             return 'Crosshair color set'
         }));
@@ -104,13 +113,38 @@ function componentToHex(c: number): string {
     return hex.length === 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r: number, g: number, b: number): string {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+function invertColor(hex: string) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    let r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + padZero(r) + padZero(g) + padZero(b);
 }
 
-function invertRGB(r: number, g: number, b: number, a?: number): string {
-    const rgb = [r, g, b, a].map((value, index) => {
-        return index === 3 ? (a !== undefined ? 1 - Number(value) : 1) : 255 - Number(value);
-    });
-    return rgbToHex(rgb[0], rgb[1], rgb[2]);
+function padZero(str: string) {
+    const len = 2;
+    const zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+}
+
+function colorNameToHex(name: string): null | string {
+    name = name.toLowerCase();
+    const colorNames = ['aliceblue','antiquewhite','aqua','aquamarine','azure','beige','bisque','black','blanchedalmond','blue','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgrey','darkgreen','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkslategrey','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dimgrey','dodgerblue','firebrick','floralwhite','forestgreen','fuchsia','gainsboro','ghostwhite','gold','goldenrod','gray','grey','green','greenyellow','honeydew','hotpink','indianred','indigo','ivory','khaki','lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan','lightgoldenrodyellow','lightgray','lightgrey','lightgreen','lightpink','lightsalmon','lightseagreen','lightskyblue','lightslategray','lightslategrey','lightsteelblue','lightyellow','lime','limegreen','linen','magenta','maroon','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','navy','oldlace','olive','olivedrab','orange','orangered','orchid','palegoldenrod','palegreen','paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue','purple','rebeccapurple','red','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell','sienna','silver','skyblue','slateblue','slategray','slategrey','snow','springgreen','steelblue','tan','teal','thistle','tomato','turquoise','violet','wheat','white','whitesmoke','yellow','yellowgreen'];
+    const colorHexs = ['f0f8ff','faebd7','00ffff','7fffd4','f0ffff','f5f5dc','ffe4c4','000000','ffebcd','0000ff','8a2be2','a52a2a','deb887','5f9ea0','7fff00','d2691e','ff7f50','6495ed','fff8dc','dc143c','00ffff','00008b','008b8b','b8860b','a9a9a9','a9a9a9','006400','bdb76b','8b008b','556b2f','ff8c00','9932cc','8b0000','e9967a','8fbc8f','483d8b','2f4f4f','2f4f4f','00ced1','9400d3','ff1493','00bfff','696969','696969','1e90ff','b22222','fffaf0','228b22','ff00ff','dcdcdc','f8f8ff','ffd700','daa520','808080','808080','008000','adff2f','f0fff0','ff69b4','cd5c5c','4b0082','fffff0','f0e68c','e6e6fa','fff0f5','7cfc00','fffacd','add8e6','f08080','e0ffff','fafad2','d3d3d3','d3d3d3','90ee90','ffb6c1','ffa07a','20b2aa','87cefa','778899','778899','b0c4de','ffffe0','00ff00','32cd32','faf0e6','ff00ff','800000','66cdaa','0000cd','ba55d3','9370db','3cb371','7b68ee','00fa9a','48d1cc','c71585','191970','f5fffa','ffe4e1','ffe4b5','ffdead','000080','fdf5e6','808000','6b8e23','ffa500','ff4500','da70d6','eee8aa','98fb98','afeeee','db7093','ffefd5','ffdab9','cd853f','ffc0cb','dda0dd','b0e0e6','800080','663399','ff0000','bc8f8f','4169e1','8b4513','fa8072','f4a460','2e8b57','fff5ee','a0522d','c0c0c0','87ceeb','6a5acd','708090','708090','fffafa','00ff7f','4682b4','d2b48c','008080','d8bfd8','ff6347','40e0d0','ee82ee','f5deb3','ffffff','f5f5f5','ffff00','9acd32'];
+    const index = colorNames.indexOf(name);
+    if (index < 0) {
+       return null;
+    }
+    return '#' + colorHexs[index];
 }
