6054f40e520f6b4f3467a26501b0d6bad3e510ff
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-12-06 03:54:45 -0500

    ng serve works

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vite';
-import analog from '@analogjs/platform';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  build: {
-    target: ['es2020'],
-  },
-  resolve: {
-    mainFields: ['module'],
-  },
-  plugins: [
-    analog({
-      ssr: false,
-      static: true,
-      prerender: {
-        routes: [],
-      },
-    }),
-  ],
-}));
+export default defineConfig(async () => {
+  const analog = (await import('@analogjs/platform')).default;
+
+  return {
+    build: {
+      target: ['es2020'],
+    },
+    resolve: {
+      mainFields: ['module'],
+    },
+    plugins: [
+      analog({
+        ssr: false,
+        static: true,
+        prerender: {
+          routes: [],
+        },
+      }),
+    ],
+  };
+});
