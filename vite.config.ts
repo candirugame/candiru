cd5ec4298d4c7ec80acc9b131a1942838b4192e2
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2024-12-06 03:19:29 -0500

    initial angular setup?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno()],
+  plugins: [],
   build: {
     chunkSizeWarningLimit: 1500,
-  }
+  },
 })
