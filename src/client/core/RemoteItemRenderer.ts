48abc8b0a183707ee67a1225ecaf4d85f9b5ae22
AustinJMann <77241134+AustinJMann@users.noreply.github.com>
2025-01-26 00:45:55 -0500

    spelling

--- src/client/core/RemoteItemRenderer.ts
+++ src/client/core/RemoteItemRenderer.ts
@@ -6,7 +6,7 @@ import { BananaGun } from '../items/BananaGun.ts';
 import { FishGun } from '../items/FishGun.ts';
 import { Pipe } from '../items/Pipe.ts';
 import { FlagItem } from '../items/FlagItem.ts';
-import { ShotHander } from './ShotHander.ts';
+import { ShotHandler } from './ShotHander.ts';
 
 // Custom types
 type Vector3Data = {
