5a5834a1a256532395c12e71115fcfca18789b52
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2025-01-27 02:29:10 -0500

    docs: add social links to main menu

--- src/app/ui/menu.component.ts
+++ src/app/ui/menu.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { SettingsComponent } from './settings.component.ts';
 import { BrowseComponent } from './browse.component.ts';
 
 @Component({
 	selector: 'app-menu',
 	standalone: true,
-	imports: [CommonModule, SettingsComponent, BrowseComponent],
+	imports: [CommonModule, SettingsComponent, BrowseComponent, NgOptimizedImage],
 	template: `
 		<div *ngIf="visible" class="fixed inset-0 bg-gray-900/80 backdrop-blur-sm z-50"
 			 (click)="onBackdropClick($event)">
@@ -18,9 +18,37 @@ import { BrowseComponent } from './browse.component.ts';
 				<div *ngIf="activePage === 'main'">
 					<h2 class="text-xl text-gray-100 mb-4">candiru</h2>
 
-					<button class="btn-menu" (click)="navigate('play')">play</button><br>
-					<button class="btn-menu" (click)="navigate('settings')">settings</button><br>
+					<button class="btn-menu" (click)="navigate('play')">play</button>
+					<br>
+					<button class="btn-menu" (click)="navigate('settings')">settings</button>
+					<br>
 					<button class="btn-menu" (click)="navigate('browse')">servers</button>
+
+
+					<div class="mt-4 flex gap-0">
+
+						<a href="https://github.com/candirugame/candiru" target="_blank" rel="noopener noreferrer"
+						   class="p-1.5 inline-flex items-center opacity-75 hover:opacity-100 transition-opacity hover:bg-gray-700/50 rounded-md">
+							<img ngSrc="/icons/github-mark-white.svg" class="h-6 w-6" width="24" height="24" alt="GitHub">
+						</a>
+						<a href="https://candiru.xyz" target="_blank" rel="noopener noreferrer"
+						   class="p-1.5 inline-flex items-center opacity-75 hover:opacity-100 transition-opacity hover:bg-gray-700/50 rounded-md">
+							<img ngSrc="/icons/link-solid.svg" class="h-6 w-6" width="24" height="24"
+								 alt="Candiru">
+						</a>
+						<a href="https://discord.gg/7tjBDYaBVn" target="_blank" rel="noopener noreferrer"
+						   class="p-1.5 inline-flex items-center opacity-75 hover:opacity-100 transition-opacity hover:bg-gray-700/50 rounded-md">
+							<img ngSrc="/icons/discord-mark-white.svg" class="h-6 w-6" width="24" height="24"
+								 alt="Discord">
+						</a>
+						<a href="mailto:team@candiru.xyz" target="_blank" rel="noopener noreferrer"
+						   class="p-1.5 inline-flex items-center opacity-75 hover:opacity-100 transition-opacity hover:bg-gray-700/50 rounded-md">
+							<img ngSrc="/icons/envelope-solid.svg" class="h-6 w-6" width="24" height="24"
+								 alt="Email">
+						</a>
+
+					</div>
+
 				</div>
 
 				<app-settings *ngIf="activePage === 'settings'"
